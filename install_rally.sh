164b35aec4e698fcb8b7846d891daea9cad97491
Yevgeniy <yshapovalov@mirantis.com>
2017-06-26 15:04:04 +0300

    fix cp

--- install_rally.sh
+++ install_rally.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo mkdir /var/lib/rally_container
 sudo chown 65500 /var/lib/rally_container
-cp ./{openrc, disable_glance_v1.conf, run_test.sh} /var/lib/rally_container
-chmod +x /var/lib/rally_container/run_test.sh
+cp -t /var/lib/rally_container openrc disable_glance_v1.conf  run_tests.sh
+sudo chmod a+x /var/lib/rally_container/run_test.sh
 docker run -it -v /var/lib/rally_container:/home/rally rallyforge/rally:0.9.1 ./run_test.sh
